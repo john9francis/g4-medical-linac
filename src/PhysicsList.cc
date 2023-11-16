309ce78f467e20c36628b0163dd37cc45f46c8cc
John Francis <john9francis@gmail.com>
2023-11-15 19:35:36 -0700

    Registered livermore physics for dose measurement

--- src/PhysicsList.cc
+++ src/PhysicsList.cc
@@ -1,26 +1,17 @@
 #include "PhysicsList.hh"
 
 #include "G4EmStandardPhysics.hh"
-#include "G4LossTableManager.hh"
-
-#include "G4EmLivermorePhysics.hh"
-#include "G4EmPenelopePhysics.hh"
-
-// Different options to test
-#include "G4EmStandardPhysics_option1.hh"
-#include "G4EmStandardPhysics_option2.hh"
-#include "G4EmStandardPhysics_option3.hh"
-#include "G4EmStandardPhysics_option4.hh"
-#include "G4EmStandardPhysicsWVI.hh"
-#include "G4EmStandardPhysicsSS.hh"
-#include "G4EmStandardPhysicsGS.hh"
-
+#include "G4EmLivermorePolarizedPhysics.hh"
 
 namespace med_linac
 {
 	PhysicsList::PhysicsList() : G4VModularPhysicsList() {
-		// Get the standard EM particles
+		// Get the standard EM physics for bremsstrahlung
 		RegisterPhysics(new G4EmStandardPhysics());
+
+		// Get the livermore polarized physics for dose measurement
+		RegisterPhysics(new G4EmLivermorePolarizedPhysics());
+
 	}
 
 	void PhysicsList::ConstructParticle(){
