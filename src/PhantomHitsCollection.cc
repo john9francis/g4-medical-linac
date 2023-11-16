0f15d986effd71a4c5405ac63fdf11deb2b6db08
John Francis <john9francis@gmail.com>
2023-11-15 17:44:07 -0700

    Added hit and hitscollection classes

---
+++ src/PhantomHitsCollection.cc
@@ -0,0 +1,37 @@
+
+#include "PhantomHitsCollection.hh"
+
+namespace med_linac {
+	PhantomHit::PhantomHit() {
+		
+		// Set the energy and position to zero
+		hEnergy = 0.;
+		hPosition = G4ThreeVector();
+	}
+
+	void PhantomHit::Print() {
+		G4cout
+			<< "Energy: "
+			<< G4BestUnit(hEnergy, "Energy")
+			<< " "
+			<< "Position: "
+			<< G4BestUnit(hPosition.getX(), "Length")
+			<< G4BestUnit(hPosition.getY(), "Length")
+			<< G4BestUnit(hPosition.getZ(), "Length")
+			<< G4endl;
+	}
+
+	// Methods to set the energy and position
+	void PhantomHit::SetEnergy(G4double e) { hEnergy = e; }
+	void PhantomHit::AddEnergy(G4double e) { hEnergy += e; }
+	void PhantomHit::SetPos(G4ThreeVector p) { hPosition = p; }
+
+
+	// Very simple HitsCollection, just a list to hold all our hits
+	PhantomHitsCollection::PhantomHitsCollection(
+		const G4String& name,
+		const G4String& collectionName)
+		: G4THitsCollection<PhantomHit>(name, collectionName) {}
+
+
+}
