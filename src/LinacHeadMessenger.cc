694631a311af2ac0d41e83623d8a4c7aa43a9ac8
John Francis <john9francis@gmail.com>
2023-12-02 11:18:03 -0700

    Got the linac head y to move! Next step is storing the position info in a singleton so we can update the primaryGenAction to know where to shoot the particles from

--- src/LinacHeadMessenger.cc
+++ src/LinacHeadMessenger.cc
@@ -48,6 +48,57 @@ namespace med_linac {
 			G4cout << "Error: Linac head not defined correctly" << G4endl;
 			return;
 		}
+
+		/*
+		For reference:
+
+		G4VPhysicalVolume* physHead = new G4PVPlacement(
+			linacHeadRotation,
+			linacHeadPos,
+			logicHead,
+			"physHead",
+			logicWorld,
+			false,
+			0);
+		*/
+
+		// Step 1: get all the necessary parameters
+		auto linacHeadRotation = fPhysLinacHead->GetObjectRotationValue();
+		auto linacHeadPos = fPhysLinacHead->GetObjectTranslation();
+		auto logicHead = fPhysLinacHead->GetLogicalVolume();
+		auto name = fPhysLinacHead->GetName();
+		auto logicMother = fPhysLinacHead->GetMotherLogical();
+
+		G4cout << "Old linac head position: " << linacHeadPos << G4endl;
+
+
+		// switch the rotation matrix to the heap
+		auto phi = linacHeadRotation.getPhi();
+		auto theta = linacHeadRotation.getTheta();
+		auto psi = linacHeadRotation.getPsi();
+		G4RotationMatrix* heapRot = new G4RotationMatrix(phi, theta, psi);
+		
+		// Step 2: Delete the old linac head
+		delete fPhysLinacHead;
+
+		// Step 3: Create a new linac head in the new position
+		G4ThreeVector newPosition = G4ThreeVector(linacHeadPos.getX(), newY, linacHeadPos.getZ());
+
+		fPhysLinacHead = new G4PVPlacement(
+			heapRot,
+			newPosition,
+			logicHead,
+			name,
+			logicMother,
+			false,
+			0);
+
+		G4cout << "New linac head position: " << fPhysLinacHead->GetObjectTranslation() << G4endl;
+
+		// Update the viewport
+		G4UImanager* UI = G4UImanager::GetUIpointer();
+		UI->ApplyCommand("/vis/viewer/rebuild");
+
 		/*
 		// get all the parameters that don't change
 		G4ThreeVector currentPos = fPhysLinacHead->GetObjectTranslation();
