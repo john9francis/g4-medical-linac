ac4c74f80a17853231495663ae2777effb4ef705
John Francis <john9francis@gmail.com>
2024-01-06 10:43:21 -0700

    Created a basic linac head messenger header file

---
+++ include/LinacHeadMessenger.hh
@@ -0,0 +1,28 @@
+#ifndef med_linac_LINAC_HEAD_MESSENGER_HH
+#define med_linac_LINAC_HEAD_MESSENGER_HH
+
+#include "G4UImessenger.hh"
+#include "G4UIcmdWithADoubleAndUnit.hh"
+
+#include "DetectorConstruction.hh"
+
+namespace med_linac {
+	class LinacHeadMessenger : public G4UImessenger {
+	public:
+		LinacHeadMessenger(DetectorConstruction* detectorConstruction);
+		~LinacHeadMessenger();
+
+		void SetNewValue(G4UIcommand* command, G4String newValues);
+		G4String GetCurrentValue(G4UIcommand* command);
+
+	
+	private:
+		DetectorConstruction* fDetectorConstruction;
+
+		G4UIcmdWithADoubleAndUnit* fLinacHeadAngle;
+	};
+
+
+}
+
+#endif // !med_linac_LINAC_HEAD_MESSENGER_HH
