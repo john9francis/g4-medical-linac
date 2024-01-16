07b8ff786768e8e8e21174f5ab881d8f8541de3c
John Francis <john9francis@gmail.com>
2024-01-15 20:17:37 -0700

    Created our pdd histogram in runaction

--- src/RunAction.cc
+++ src/RunAction.cc
@@ -2,10 +2,21 @@
 #include "RunAction.hh"
 #include "G4ThreeVector.hh"
 #include "G4UnitsTable.hh"
+#include "G4SystemOfUnits.hh"
 
 
 namespace med_linac {
 	RunAction::RunAction() {
+		// set up our histograms
+		
+		auto analysisManager = G4AnalysisManager::Instance();
+
+		analysisManager->SetDefaultFileType("root");
+		analysisManager->SetVerboseLevel(0);
+		analysisManager->SetFileName("PDD");
+
+		analysisManager->CreateH1("PDD", "PDD Graph", 100, 0, 15 * cm);
+
 
 	}
 
