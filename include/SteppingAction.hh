d81a63282585d86bb5cb4132fad8b02dce4f4f3f
John Francis <john9francis@gmail.com>
2024-01-15 17:15:18 -0700

    Fixed stepping action hh

--- include/SteppingAction.hh
+++ include/SteppingAction.hh
@@ -4,23 +4,16 @@
 #include "G4UserSteppingAction.hh"
 #include "G4LogicalVolume.hh"
 
-#include "RunAction.hh"
-
 
 namespace med_linac {
 
 
 	class SteppingAction : public G4UserSteppingAction {
 	public:
-		SteppingAction(RunAction*);
+		SteppingAction();
 		~SteppingAction();
 
 		void UserSteppingAction(const G4Step*) override;
-
-
-	private:
-		G4LogicalVolume* fPhantom = nullptr;
-		RunAction* fRunAction = nullptr;
 	};
 
 }
