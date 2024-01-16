d6361e45c56dea45839273b095403e87bbb9dce4
John Francis <john9francis@gmail.com>
2024-01-15 17:12:11 -0700

    Fixed detector construction hh

--- include/DetectorConstruction.hh
+++ include/DetectorConstruction.hh
@@ -14,12 +14,6 @@
 #include "G4SystemOfUnits.hh"
 #include "CLHEP/Units/SystemOfUnits.h"
 
-#include "G4UImessenger.hh"
-
-#include "G4UIcmdWithoutParameter.hh"
-#include "G4UIcmdWithADouble.hh"
-
-#include "LinacHeadMessenger.hh"
 
 namespace med_linac
 {
@@ -29,9 +23,7 @@ namespace med_linac
 		DetectorConstruction();
 		~DetectorConstruction() override = default;
 
-
 		G4VPhysicalVolume* Construct() override;
-		void ConstructSDandField() override;
 
 		// for detectormessenger
 		void SetLinacHeadAngle(G4ThreeVector phiThetaPsi);
@@ -44,9 +36,8 @@ namespace med_linac
 		G4ThreeVector GetLinacHeadPos() const { return *fLinacHeadPos; }
 		G4ThreeVector GetGunAnchorPos() const { return *fGunAnchorPos; }
 
-	
 	private:
-		G4VPhysicalVolume* fPhysLinacHead = nullptr;
+		G4VPhysicalVolume* fLinacHead = nullptr;
 		G4VPhysicalVolume* fParticleGunAnchor1 = nullptr;
 
 		G4ThreeVector* fLinacHeadPos;
@@ -59,7 +50,6 @@ namespace med_linac
 		G4ThreeVector* fGunAnchorPos;
 
 		void MoveLinacHead();
-
 	};
 }
 
