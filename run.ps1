7c1ecaa55b2984c4409c6e72921c8685b8499ebf
John Francis <john9francis@gmail.com>
2024-01-30 19:24:19 -0700

    created a script to easily test pdd

--- run.ps1
+++ run.ps1
@@ -1,4 +1,18 @@
-cd build/release
-.\g4_medical_linac rotate.mac
+# Change directory to the build folder
+cd build
+
+# Run CMake
+cmake ..
+
+# Build the project using MSBuild
+msbuild g4_medical_linac.sln
+
+# Change directory to the release folder
+cd release
+
+# Run the executable
+.\g4_medical_linac pdd.mac
+
+# Go back to the root directory
 cd ..
 cd ..
