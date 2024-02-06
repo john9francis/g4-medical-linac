f89eca1d82bbb451822f18626d84bd57fd911a7d
John Francis <john9francis@gmail.com>
2024-02-06 00:47:24 -0700

    made a ps1 file to run all outputs

---
+++ all_run.ps1
@@ -0,0 +1,21 @@
+# Change directory to the build folder
+cd build
+
+# Run CMake
+cmake ..
+
+# Build the project using MSBuild
+msbuild g4_medical_linac.sln
+
+# Change directory to the release folder
+cd release
+
+# Run the executable with all the outputs
+.\g4_medical_linac basicGraphs.mac
+.\g4_medical_linac FFFgraphs.mac
+.\g4_medical_linac heatMap.mac
+.\g4_medical_linac FFFheatMap.mac
+
+# Go back to the root directory
+cd ..
+cd ..
